81e165ae6b1ffdad4c952a5388dbd8b71586c105
Himanshu Pandey <himanshugithub6201@gmail.com>
2022-12-03 21:16:11 +0530

    AppIntro and Background component

---
+++ Web3Frontend/src/Background.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import './Background.css';
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
+function Background() {
+  return (
+    <div class="wrapper">
+      <h1>Welcome to the World Of Web3Store <br></br> <h6>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially </h6> </h1>
+
+      <div><span class="dot"></span></div>
+      <div><span class="dot"></span></div>
+      <div><span class="dot"></span></div>
+      <div><span class="dot"></span></div>
+      <div><span class="dot"></span></div>
+      <div><span class="dot"></span></div>
+      <div><span class="dot"></span></div>
+      <div><span class="dot"></span></div>
+      <div><span class="dot"></span></div>
+      <div><span class="dot"></span></div>
+      <div><span class="dot"></span></div>
+      <div><span class="dot"></span></div>
+      <div><span class="dot"></span></div>
+      <div><span class="dot"></span></div>
+      <div><span class="dot"></span></div>
+    </div>
+  )
+}
+export default Background;
